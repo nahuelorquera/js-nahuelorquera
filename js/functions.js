34c7d8c0b493ac4b4b607551b593f41cf545bbf1
Nahuel Orquera <nahuelorquera39@gmail.com>
2022-05-05 12:33:47 -0300

    nueva entrega

--- js/functions.js
+++ js/functions.js
@@ -17,6 +17,9 @@ while (contraseniaR != contraseniaI){
 }  */
 
 
+let monto = parseInt(prompt("Ingrese monto a pagar"));
+let cuotas = prompt ("Ingrese en cuantas cuotas (1,2,6,12)");
+
 function calcularCuotas(monto,cuotas){
     let unaCuota = monto;
     let dosCuotas = (monto / 2) + 100;
@@ -24,17 +27,18 @@ function calcularCuotas(monto,cuotas){
     let doceCuotas = (monto / 12) + 400;
      ;
     if(cuotas == "1"){
-        return console.log ("Usted debe pagar " + monto + " en una sola cuota")
+        return alert("Usted debe pagar " + monto + " en una sola cuota")
     }
     else if (cuotas == "2"){
-        return console.log("Usted debe pagar 2 cuotas de " + dosCuotas )
+        return alert("Usted debe pagar 2 cuotas de " + dosCuotas )
     }else if(cuotas == "6"){
-        return console.log("Usted debe pagar 6 cuotas de " + seisCuotas )
+        return alert("Usted debe pagar 6 cuotas de " + seisCuotas )
     }else if(cuotas == "12"){
-        return console.log("Usted debe pagar 12 cuotas de " + doceCuotas )
+        return alert("Usted debe pagar 12 cuotas de " + doceCuotas )
     }
+    
 }
 
+alert(calcularCuotas(monto,cuotas))
 
-calcularCuotas(12000,6)
 
